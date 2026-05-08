79c96f37f727a7cef5919cb0efb6c31efb2e6c2f
ryeon-xx <rlarkgus3415@gmail.com>
2026-05-08 16:34:16 +0900

    Diary 만들기 & MySQL DB 연동

---
+++ src/test/DiaryApp.java
@@ -0,0 +1,16 @@
+package test;
+
+import javax.swing.*;
+
+public class DiaryApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            new DiaryUI().setVisible(true);
+        });
+    }
+}
